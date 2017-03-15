bd3b0321faff9a3c4ce8d0f87de863c0f93df98e
mara-aspit <mara@aspit.dk>
2017-03-15 09:56:25 +0100

    Added type TypeChecker, with one method.

--- AspIT.Utils/Class1.cs
+++ AspIT.Utils/Class1.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Author: Mads Mikkel Rasmussen.
+
+using System;
 
 namespace AspIT.Utils
 {
-    public class Class1
+    /// <summary>
+    /// Checks types.
+    /// </summary>
+    public static class TypeChecker
     {
+        /// <summary>
+        /// Determines is a specified type i a numeric type.
+        /// </summary>
+        /// <param name="type">The type to check.</param>
+        /// <returns>A <see cref="bool"/> that indicates whether or not the specified type was a numeric type.</returns>
+        public static bool IsNumericType(Type type)
+        {
+            return false;
+        }
     }
 }
