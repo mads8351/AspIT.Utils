d8297a2e57e40c1196d80294c7dc1794d8c3f9c2
FrederikAspIT <fred159a@edu.campusvejle.dk>
2017-03-15 10:59:04 +0100

    changes stuff

--- AspIT.Utils/Class1.cs
+++ AspIT.Utils/Class1.cs
@@ -16,7 +16,9 @@ namespace AspIT.Utils
         /// <returns>A <see cref="bool"/> that indicates whether or not the specified type was a numeric type.</returns>
         public static bool IsNumericType(Type type)
         {
-            return false;
+            if(type == null)
+                return false;
+            else return false;
         }
     }
 }
