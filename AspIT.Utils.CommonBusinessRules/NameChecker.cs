d26e44c100c58b50d7392251eb4d162fb4aa01d0
FrederikAspIT <fred159a@edu.campusvejle.dk>
2017-03-17 09:40:24 +0100

    added

---
+++ AspIT.Utils.CommonBusinessRules/NameChecker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspIT.Utils.CommonBusinessRules
+{
+    public static class NameChecker
+    {
+        public static bool IsValidName(string name)
+        {
+
+            string[] exceptions = { "von", "di", "la" };
+
+            foreach(Char c in name)
+            {
+
+                if(!char.IsLetter(c) &&  c != '-')
+                {
+                    return false;
+                }
+
+            }
+
+            return true;
+        }
+    }
+}
