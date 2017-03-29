234ff63b71d35ce8329d77964dafbf8ebf27e786
mads8351 <mads8351@edu.campusvejle.dk>
2017-03-29 10:55:59 +0200

    vaildation of firstname

---
+++ AspIT.Utils.VbCommonBusinessRules.UnitTest/UnitTest1.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()> Public Class VBNameCheckerTests
+
+  <TestMethod()> Public Sub ValidNamePasses()
+    Dim PR As PersonRules = New PersonRules
+
+    Dim expected As Boolean = True
+    Dim actual As Boolean
+    Dim testString As String = "Åajbritt"
+
+    actual = PR.isValidName(testString)
+
+    Assert.AreEqual(expected, actual)
+  End Sub
+
+  <TestMethod()> Public Sub NotValidNamePasses()
+    Dim PR As PersonRules = New PersonRules
+
+    Dim expected As Boolean = False
+    Dim actual As Boolean
+    Dim testString As String = "majbritt"
+
+    actual = PR.isValidName(testString)
+
+    Assert.AreEqual(expected, actual)
+  End Sub
+
+End Class
