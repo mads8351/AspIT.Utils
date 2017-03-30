4b9124aee7991de3110c68c60d74b110208a80ee
mads8351 <mads8351@edu.campusvejle.dk>
2017-03-30 14:40:00 +0200

    dead stop

--- AspIT.Utils.VbCommonBusinessRules.UnitTest/UnitTest1.vb
+++ AspIT.Utils.VbCommonBusinessRules.UnitTest/UnitTest1.vb
@@ -1,7 +1,21 @@
 ﻿Imports System.Text
 Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports AspIT.Utils.Collections
+Imports System.IO
 
 <TestClass()> Public Class VBNameCheckerTests
+
+  <TestMethod()> Public Sub ValidNamesPasses()
+    Dim testValues As List(Of String) = New List(Of String)
+    Dim reader As StreamReader = New StreamReader("C:\SourceCodes\Textfiles\Names.txt")
+    Dim line As String = Nothing
+    While ((line = reader.ReadLine))
+
+    End While
+    Dim TD As TestDictionary(Of String, Boolean) = New TestDictionary(Of String, Boolean)
+
+
+  End Sub
 
   <TestMethod()> Public Sub ValidNamePasses()
     Dim PR As PersonRules = New PersonRules
